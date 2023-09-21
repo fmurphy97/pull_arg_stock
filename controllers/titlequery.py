45be523568276675fd71b2e2a81d8a4677ec32c6
fmurphy97 <federicoianmurphy@gmail.com>
2023-09-21 10:18:52 -0300

    Add function to get all cedear data

--- controllers/titlequery.py
+++ controllers/titlequery.py
@@ -4,7 +4,7 @@ from controllers.token import Token
 from utils.constants import URL_API
 
 
-class Titles:
+class TitleQuery:
 
     def __init__(self, token: Token):
         self.token = token
@@ -20,15 +20,20 @@ class Titles:
             f'{URL_API}/{country}/Titulos/Cotizacion/Instrumentos', headers=headers)
         return response.json()
 
+    def get_instrument_data(self, instrument_type, country):
+        api_key = self.token.get_token()
+        headers = {'Authorization': f'Bearer {api_key}'}
+        response = requests.get(
+            f'{URL_API}Cotizaciones/{instrument_type}/{country}/Todos?'
+            f'cotizacionInstrumentoModel.instrumento={instrument_type}'
+            f'&cotizacionInstrumentoModel.pais={country}'
+            f'&api_key={api_key}',
+            headers=headers)
+        return response.json()
+
     def get_historical_series(self, market, symbol, date_from, date_to):
         headers = {'Authorization': f'Bearer {self.token.get_token()}'}
         response = requests.get(
             f'{URL_API}/{market}/Titulos/{symbol}/Cotizacion/seriehistorica/{date_from}/{date_to}/ajustada',
             headers=headers)
         return response.json()
-
-
-if __name__ == "__main__":
-    from controllers.token import Token
-    tkn = Token()
-    titles_collector = Titles(tkn)
