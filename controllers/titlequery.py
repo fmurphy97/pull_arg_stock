37aa83e2c5fd24775d76cd1ad531f864ea0fd2b0
fmurphy97 <federicoianmurphy@gmail.com>
2023-09-27 17:46:14 -0300

    Remove unnecessary functions, add query for ticker and period

--- controllers/titlequery.py
+++ controllers/titlequery.py
@@ -9,25 +9,19 @@ class TitleQuery:
     def __init__(self, token: Token):
         self.token = token
 
-    def get_titles(self):
-        headers = {'Authorization': f'Bearer {self.token.get_token()}'}
-        response = requests.get(f'{URL_API}/Titulos/FCI', headers=headers)
-        return response.json()
-
-    def get_quotes(self, country):
-        headers = {'Authorization': f'Bearer {self.token.get_token()}'}
-        response = requests.get(
-            f'{URL_API}/{country}/Titulos/Cotizacion/Instrumentos', headers=headers)
-        return response.json()
-
     def get_instrument_data(self, instrument_type, country):
         api_key = self.token.get_token()
         headers = {'Authorization': f'Bearer {api_key}'}
         response = requests.get(
-            f'{URL_API}Cotizaciones/{instrument_type}/{country}/Todos?'
-            f'cotizacionInstrumentoModel.instrumento={instrument_type}'
-            f'&cotizacionInstrumentoModel.pais={country}'
-            f'&api_key={api_key}',
+            f'{URL_API}/Cotizaciones/{instrument_type}/{country}/Todos?',
+            headers=headers)
+        return response.json()
+
+    def get_ticker_data(self, mercado, symbol, period):
+        api_key = self.token.get_token()
+        headers = {'Authorization': f'Bearer {api_key}'}
+        response = requests.get(
+            f'{URL_API}/{mercado}/Titulos/{symbol}/CotizacionDetalleMobile/{period}',
             headers=headers)
         return response.json()
 
