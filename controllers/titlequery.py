7a53d51e3d0ae1d0722c11b2cb3332692e4a36de
fmurphy97 <federicoianmurphy@gmail.com>
2023-09-28 09:09:54 -0300

    Assert that inputs are in the possible parameters

--- controllers/titlequery.py
+++ controllers/titlequery.py
@@ -1,7 +1,7 @@
 import requests
 
 from controllers.token import Token
-from utils.constants import URL_API
+from utils.constants import URL_API, MARKETS, PERIODS, INSTRUMENT_TYPES, COUNTRIES
 
 
 class TitleQuery:
@@ -10,6 +10,11 @@ class TitleQuery:
         self.token = token
 
     def get_instrument_data(self, instrument_type, country):
+        if instrument_type not in INSTRUMENT_TYPES:
+            return f"The instrument type {instrument_type} is not in {INSTRUMENT_TYPES}"
+        if country not in COUNTRIES:
+            return f"The country {country} is not in {COUNTRIES}"
+
         api_key = self.token.get_token()
         headers = {'Authorization': f'Bearer {api_key}'}
         response = requests.get(
@@ -17,15 +22,23 @@ class TitleQuery:
             headers=headers)
         return response.json()
 
-    def get_ticker_data(self, mercado, symbol, period):
+    def get_ticker_data(self, market, symbol, period):
+        if market not in MARKETS:
+            return f"The market {market} is not in {MARKETS}"
+        if period not in PERIODS:
+            return f"The period {period} is not in {PERIODS}"
+
         api_key = self.token.get_token()
         headers = {'Authorization': f'Bearer {api_key}'}
         response = requests.get(
-            f'{URL_API}/{mercado}/Titulos/{symbol}/CotizacionDetalleMobile/{period}',
+            f'{URL_API}/{market}/Titulos/{symbol}/CotizacionDetalleMobile/{period}',
             headers=headers)
         return response.json()
 
     def get_historical_series(self, market, symbol, date_from, date_to):
+        if market not in MARKETS:
+            return f"The market {market} is not in {MARKETS}"
+
         headers = {'Authorization': f'Bearer {self.token.get_token()}'}
         response = requests.get(
             f'{URL_API}/{market}/Titulos/{symbol}/Cotizacion/seriehistorica/{date_from}/{date_to}/ajustada',
--- utils/constants.py
+++ utils/constants.py
@@ -1,12 +1,11 @@
 URL_TOKEN = 'https://api.invertironline.com/token'
 GRANT_TYPE = 'password'
 URL_API = 'https://api.invertironline.com/api/v2/'
+
 COUNTRIES = ['argentina', 'estados_Unidos']
 MARKETS = ['bcBA', 'nYSE', 'nASDAQ', 'aMEX', 'bCS', 'rOFX']
-TOOLS = ['acciones', 'bonos', 'opciones', 'monedas',
-         'cauciones', 'cHPD', 'futuros', 'aDRs']
-INVESTMENT = ['plazo_fijo_pesos', 'plazo_fijo_dolares', 'renta_fija_pesos', 'renta_fija_dolares',
-              'renta_mixta_pesos', 'renta_mixta_dolares', 'renta_variable_pesos', 'renta_variable_dolares']
-SYMBOLS = ['ALUA', 'BBAR', 'BMA', 'BYMA', 'CEPU', 'COME', 'CRES', 'CVH', 'EDN', 'GGAL',
-           'MIRG', 'PAMP', 'SUPV', 'TECO2', 'TGNO4', 'TGSU2', 'TRAN', 'TXAR', 'VALO', 'YPFD']
+PERIODS = ["t0", "t1", "t2"]
+INSTRUMENT_TYPES = ['acciones', 'opciones', 'monedas', 'cedears', 'titulosPublicos' 'cauciones', 'cHPD', 'futuros',
+                    'aDRs', 'obligacionesNegociables', 'letras']
+INSTRUMENT_TYPES_V2 = ['acciones', 'bonos', 'opciones', 'monedas', 'cauciones', 'cHPD', 'futuros', 'aDRs']
 STATE = ['todas', 'pendientes', 'terminadas', 'canceladas']
