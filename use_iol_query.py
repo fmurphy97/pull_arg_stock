7fde29a5665da92bb26eba79167f7cd70c5ccc7c
fmurphy97 <federicoianmurphy@gmail.com>
2023-10-24 23:16:57 -0300

    wrap iol into functions to call externally

--- use_iol_query.py
+++ use_iol_query.py
@@ -1,56 +1,68 @@
+import pandas as pd
 from iolApi.token import Token
 from iolApi.titlequery import TitleQuery
-from utils.clean_cedear_data import *
+from utils.clean_cedear_data import split_into_countries, calculate_mep
 
-# Create token and title query instances
-token = Token()
-title_query = TitleQuery(token)
 
-# Query any instrument
-queried_data = title_query.get_instrument_data(instrument_type="cedears", country="argentina")
+def query_data():
+    # Create token and title query instances
+    token = Token()
+    title_query = TitleQuery(token)
 
-# Get it into a df
-queried_df = pd.DataFrame(queried_data['titulos'])
-# Expand the puntas column
-queried_df = queried_df[~queried_df["puntas"].isna()]
-queried_df[[key for key in queried_df['puntas'].iloc[0].keys()]] = queried_df['puntas'].apply(lambda x: pd.Series(x))
+    # Query any instrument
+    queried_data = title_query.get_instrument_data(instrument_type="cedears", country="argentina")
 
-es_to_en_col_mapping = {
-    'simbolo': "symbol",
-    'apertura': "open",
-    'maximo': "dayMax",
-    'minimo': "dayMin",
-    'ultimoCierre': "close",
-    'volumen': "volume",
-    'mercado': "exchange",
-    'moneda': "currency",
-    'descripcion': "shortName",
-    'cantidadCompra': "askSize",
-    'precioCompra': "ask",
-    'precioVenta': "bid",
-    'cantidadVenta': "bidSize"
-}
+    # Get it into a df
+    df = pd.DataFrame(queried_data['titulos'])
 
-queried_df.rename(columns=es_to_en_col_mapping, inplace=True)
-queried_df = queried_df[list(es_to_en_col_mapping.values())]
+    return df
 
-queried_df['currency'] = queried_df['currency'].replace({"1": 'ARS', "2": 'USD'})
-queried_df['exchange'] = queried_df['exchange'].replace({"1": 'BUE'})
-queried_df['symbol'] = queried_df['symbol'] + '.BA'
 
-cedear_data = pd.read_csv("data/inputs/cedear_ratios.csv")
+def format_output_df(df):
+    # Expand the puntas column
+    df = df[~df["puntas"].isna()]
+    df[[key for key in df['puntas'].iloc[0].keys()]] = df['puntas'].apply(lambda x: pd.Series(x))
 
-# df_full = all_calculations(df=queried_df, species_data=cedear_data)
-my_dict = {}
-for _, row in cedear_data.iterrows():
-    my_dict[row['symbol_arg']] = row['symbol']
-    my_dict[row['symbol_arg_usd']] = row['symbol']
+    # Re-Map english to spanish columns
+    es_to_en_col_mapping = {'simbolo': "symbol", 'apertura': "open", 'maximo': "dayMax", 'minimo': "dayMin",
+                            'ultimoCierre': "close", 'volumen': "volume", 'mercado': "exchange", 'moneda': "currency",
+                            'descripcion': "shortName", 'cantidadCompra': "bidSize", 'precioCompra': "bid",
+                            'precioVenta': "ask", 'cantidadVenta': "askSize"
+                            }
+    df.rename(columns=es_to_en_col_mapping, inplace=True)
 
-queried_df['base_symbol'] = queried_df['symbol'].map(my_dict)
-df2 = split_into_countries(queried_df, cedear_data)
-df3 = calculate_mep(df2)
+    # Keep only relevant columns
+    df = df[list(es_to_en_col_mapping.values())]
 
-cols = ["base_symbol", "shortName_D_BA", "open_BA", "bid_BA", "ask_BA", "open_D_BA", "bid_D_BA", "ask_D_BA",
-        "volume_BA",
-        "volume_D_BA", "MEP", "USD/ARS ask", "USD/ARS bid"]
-df3[cols].rename(columns={"shortName_D_BA": "shortName"}).to_csv("data/outputs/df_mep.csv", index=False)
+    # Update currencies
+    df['currency'] = df['currency'].replace({"1": 'ARS', "2": 'USD'})
+    df['exchange'] = df['exchange'].replace({"1": 'BUE'})
+    df['symbol'] = df['symbol'] + '.BA'
+
+    return df
+
+
+def join_local_vs_foreign_asset(df):
+    asset_ratios = pd.read_csv("data/inputs/cedear_ratios.csv")
+
+    my_dict = {}
+    for _, row in asset_ratios.iterrows():
+        my_dict[row['symbol_arg']] = row['symbol']
+        my_dict[row['symbol_arg_usd']] = row['symbol']
+
+    df['base_symbol'] = df['symbol'].map(my_dict)
+    df2 = split_into_countries(df, asset_ratios)
+    df3 = calculate_mep(df2)
+
+    return df3
+
+
+def update_iol_data():
+    queried_df = query_data()
+    queried_df_formatted = format_output_df(queried_df)
+    final_queried_df = join_local_vs_foreign_asset(queried_df_formatted)
+
+    cols = ["base_symbol", "shortName_D_BA", "open_BA", "bid_BA", "ask_BA", "open_D_BA", "bid_D_BA", "ask_D_BA",
+            "volume_BA", "volume_D_BA", "MEP", "USD/ARS ask", "USD/ARS bid"]
+    final_queried_df[cols].rename(columns={"shortName_D_BA": "shortName"}).to_csv("data/outputs/df_mep.csv",
+                                                                                  index=False)
