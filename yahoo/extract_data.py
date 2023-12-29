b283c826f8a78af8298df72130b790a608c859b3
fmurphy97 <federicoianmurphy@gmail.com>
2023-12-29 18:27:20 -0300

    Clean yahoo data

--- yahoo/extract_data.py
+++ yahoo/extract_data.py
@@ -1,35 +1,55 @@
 from tqdm import tqdm
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
+from utils.symbol_data_extractor import SymbolDataExtractor
 
 
-def query_symbol_data_yf(species_data):
-    """Iterate over all the symbols and the combinations, and pull the data"""
-    missing_symbols = []
-    data = {}
+class YahooDataExtractor(SymbolDataExtractor):
 
-    input_species_data = species_data[species_data["include"]].to_dict(orient="records")
+    def __init__(self):
+        super().__init__()
+        self.col_mapping = {
+            "symbol": 'symbol',
+            "close": 'Adj Close',
+            "dayMax": 'High',
+            "dayMin": 'Low',
+            "open": 'Open',
+            "volume": 'Volume'}
 
-    for symbol_info in tqdm(input_species_data):
-        base_symbol = symbol_info["symbol"]
-        for symbol_type in ['symbol', 'symbol_arg', 'symbol_arg_usd']:
-            current_symbol = str(symbol_info[symbol_type])
-            try:
-                yf_ticker = yf.Ticker(current_symbol)
-                retrieved_ticker_info = yf_ticker.info
-                retrieved_ticker_info["base_symbol"] = base_symbol
+        self.queried_df = None
 
-                data[current_symbol] = yf_ticker.info
-            except:
-                missing_symbols.append(current_symbol)
-    print("No data for: ", missing_symbols)
-    return pd.DataFrame.from_dict(data, orient="index")
+    def get_data(self):
+        cedear_ratios = pd.read_excel("../data/cedear_ratios_reloaded.xlsx", sheet_name="cedear")
+        cedear_ratios = cedear_ratios[
+            (cedear_ratios["type"].isin(["base", "dolar"]))
+            & (cedear_ratios["can_use"] == 1)
+            ]
 
+        today_date = datetime.now().date()
+        symbols = [s + ".BA" for s in set(cedear_ratios["symbol"])]
+        queried_df = yf.download(symbols, period="1d", interval="1d")
+        queried_df.index = pd.to_datetime(queried_df.index)
+        filtered_df = queried_df[queried_df.index.date == today_date]
+        stacked_df = filtered_df.stack(level=0).T.reset_index().droplevel(0, axis=1)
+        stacked_df.rename(columns={'': 'symbol'}, inplace=True)
 
-if __name__ == "__main__":
-    cedear_ratios = pd.read_excel("../data/cedear_ratios_reloaded.xlsx", sheet_name="cedear")
-    cedear_ratios = cedear_ratios[
-        (cedear_ratios["type"].isin(["base", "dolar"]))
-        & (cedear_ratios["can_use"] == 1)
-        ]
-    bla = yf.download(["SPY", "AAPL"], period="1d", interval="1d")
+        self.queried_df = stacked_df
+
+    def format_output(self):
+        self.queried_df['symbol'] = self.queried_df['symbol'].str.split('.').str[0]
+        self.queried_df['exchange'] = "BUE"
+        self.queried_df['settlementPeriod'] = 48
+
+        # Add currency
+        cedear_ratios = pd.read_excel("../data/cedear_ratios_reloaded.xlsx", sheet_name="cedear")
+        self.queried_df = self.queried_df.merge(cedear_ratios[["symbol", "currency"]], on="symbol", how="left")
+
+        # The following data is not available
+        self.queried_df['ask'] = None
+        self.queried_df['bid'] = None
+        self.queried_df['askSize'] = None
+        self.queried_df['bidSize'] = None
+
+        # Keep only relevant columns
+        self.clean_df = self.queried_df
