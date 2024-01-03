15c264d31d8dde575052fd11c4125dbc4b34b2f6
fmurphy97 <federicoianmurphy@gmail.com>
2024-01-03 13:54:48 -0300

    split the data source

--- yahoo/extract_data.py
+++ yahoo/extract_data.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 class YahooDataExtractor(SymbolDataExtractor):
 
-    def __init__(self):
+    def __init__(self, symbols_to_query=None):
         super().__init__()
         self.col_mapping = {'symbol': 'symbol',
                             'Adj Close': 'close',
@@ -15,19 +15,24 @@ class YahooDataExtractor(SymbolDataExtractor):
                             'Low': 'dayMin',
                             'Open': 'open',
                             'Volume': 'volume'}
+        self.symbols_to_query = symbols_to_query
 
-
-    def get_data(self):
+    def get_cedear_symbols(self):
         ratios_data_path = Path(__file__).parent.parent.joinpath("data", "cedear_ratios_reloaded.xlsx")
         cedear_ratios = pd.read_excel(ratios_data_path, sheet_name="cedear")
         cedear_ratios = cedear_ratios[
             (cedear_ratios["type"].isin(["base", "dolar"]))
             & (cedear_ratios["can_use"] == 1)
             ]
+        symbols_to_query = set(cedear_ratios["symbol"])
+        symbols = {s + ".BA" for s in symbols_to_query}
+        self.symbols_to_query = symbols
 
+    def get_data(self):
+        if self.symbols_to_query is None:
+            self.get_cedear_symbols()
         today_date = datetime.now().date()
-        symbols = [s + ".BA" for s in set(cedear_ratios["symbol"])]
-        queried_df = yf.download(symbols, period="1d", interval="1d")
+        queried_df = yf.download(self.symbols_to_query, period="1d", interval="1d")
         queried_df.index = pd.to_datetime(queried_df.index)
         filtered_df = queried_df[queried_df.index.date == today_date]
         stacked_df = filtered_df.stack(level=0).T.reset_index().droplevel(0, axis=1)
